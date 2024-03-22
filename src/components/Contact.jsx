6307e4d8f09708f5ae66d926903124c75397e52b
Marek Bis <marek.bis5@seznam.cz>
2024-03-22 15:42:54 +0100

    tooltip fail

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,22 @@
-// TODO ADD COPYTOCLIPBOARD API
-
+import copy from 'copy-to-clipboard';
 const Contact = () => {
+
+
+
+
   return (
     <div name='contact' className="w-full h-fit py-20 md:py-0 md:h-screen bg-[#0a192f] flex justify-center flex-col items-center p-4">
         <form action="https://getform.io/f/jbwxgvva" method="POST" className=" flex flex-col w-[1/5] gap-3">
             <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-green-500 text-gray-300 ">Kontakt</p>
-                <p className="text-gray-300 py-4">V přiloženém formuláři mě můžete kontaktovat, popřípadě na email: <span className=' hover:text-green-500'>marek.bis@seznam.cz</span></p>
+                <p className="text-4xl font-bold inline border-b-4 border-green-500 text-gray-300 ">
+                  Kontakt
+                </p>
+                <p className="text-gray-300 py-4">
+                  Konktaktovat mě můžete v přiloženém formuláři, popřípadě přímo na můj email: 
+                 <span onClick={()=>copy("marek.bis5@seznam.cz")} className='group active:text-green-500 cursor-pointer'>  
+                   marek.bis@seznam.cz
+                  </span>
+                 </p>
             </div>
             <input className="p-2 bg-[#0a192f] outline-none border-2 text-white border-green-500" type="text" name="name" placeholder="Jméno" required="required" />
             <input className="p-2 bg-[#0a192f] outline-none border-2 text-white border-green-500" type="email" name="email" placeholder="E-mail" required="required"/>
