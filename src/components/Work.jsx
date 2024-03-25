a9d54d084888cfaec630149583200b6004006334
Marek Bis <marek.bis5@seznam.cz>
2024-03-25 15:05:59 +0100

    updated text

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -39,7 +39,7 @@ const Work = () => {
             <div className="max-w-[900px] mx-auto p-4 flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-green-500">Projekty</p>
-                    <p className="py-6">Moje dosavadní mini projekty.</p>
+                    <p className="py-6">Moje dosavadní mini projekty. Více ukázek kódů můžete nalézt na mém githubu.</p>
                 </div>
                 {/* CONTAINER PROJECTS */}
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
